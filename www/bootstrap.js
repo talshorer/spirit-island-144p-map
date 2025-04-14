af5adeb123df2f8f960ce6e686277d248f9804bb
Tal Shorer <tal.shorer@gmail.com>
2025-04-14 11:31:41 +0300

    www: remove unneeded content
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- www/bootstrap.js
+++
@@ -1,3 +0,0 @@
-// XXX-tals useless?
-import("./index.js")
-  .catch(e => console.error("Error importing `index.js`:", e));
--- www/index.js
+++
@@ -1,6 +0,0 @@
-// XXX-tals useless?
-// import * as wasm from "./pkg/spirit_island_144p_map.js";
-
-console.log(wasm);
-
-wasm.app();
