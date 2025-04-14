a139bade3f526d72fb917ea41fdbd44eef5b5fcf
Tal Shorer <tal.shorer@gmail.com>
2025-04-14 11:39:58 +0300

    deploy from porcelain state
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+if [ $(git status --porcelain | wc -c) -ne 0 ]; then
+    echo "Must deploy from porcelain git state!"
+    exit 1
+fi
+
 rm -rf pkg/*
 wasm-pack build --target web --no-typescript
 git add -f pkg/
